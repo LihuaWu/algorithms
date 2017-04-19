d0b86028c3da71ed398907a1e822cbfa7ac7be8b
wulihua <wulihua@sogou-inc.com>
2017-04-19 20:55:32 +0800

    add 7etmp.

--- cf/7etmp.cpp
+++ cf/7etmp.cpp
@@ -8,20 +8,21 @@ char s[120];
 int n,m;
 int eval(int l,int r)
 {
-    for(int i=r,w=0;i-->l;)
-        if(w+=s[i]=='(',w-=s[i]==')',!w&&(s[i]=='+'||s[i]=='-'))
-        {
+    for(int i=r,w=0;i-->l;) {
+        if(w+=s[i]=='(',w-=s[i]==')',!w&&(s[i]=='+'||s[i]=='-')) {
             int L=eval(l,i),R=eval(i+1,r);
             return L&&R&&(s[i]!='-'||R>1);
         }
-    for(int i=r,w=0;i-->l;)
-        if(w+=s[i]=='(',w-=s[i]==')',!w&&(s[i]=='*'||s[i]=='/'))
-        {
+    }
+    for(int i=r,w=0;i-->l;) {
+        if(w+=s[i]=='(',w-=s[i]==')',!w&&(s[i]=='*'||s[i]=='/')) {
             int L=eval(l,i),R=eval(i+1,r);
             return L>1&&R>1&&(s[i]!='/'||R>2)?2:0;
         }
-    if(s[l]=='(')
+    }
+    if(s[l]=='(') {
         return eval(l+1,r-1)?3:0;
+    }
     string u(s+l,s+r);
     return g.count(u)?g[u]:3;
 }
